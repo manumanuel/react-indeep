9f668a89fff6efe862742aad37f5ae8b9c1abd9a
Manu Manuel <manuelmanu008@gmail.com>
2024-06-30 10:34:39 +0530

    added react components

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# create app without JSX
+# stage-1
 
-# using React.createElement("div", {}, "");
+- create app without JSX
 
-# ReactDOM.createRoot(document.getElementById("root"));
+# Parcel
+
+- Dev Build
+- automatically host on a local server
+- doing HMR (Hot Module Replacement -using file watching algorithms written in cpp)
+- it supports caching (helps in faster building of app)
+- image optimization
+- bundling & minification
+- compressing the files (by avoiding comments & white spaces)
+- consistent hashing
+- code splitting
+- differential bundling (support old browsers)
+- diagnosis
+- error handling / meaningful error suggestions
+- supports HTTPs
+- tree shaking (removes unused codes)
+- different dev & prod builds (dev build take little more time compared with prod build)
--- app.js
+++ app.js
@@ -7,6 +7,9 @@
 </div>
 */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const nested = [
   React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
@@ -27,6 +30,26 @@ const element = React.createElement(
   "Hello world from React"
 );
 console.log(element);
+
+const jsxHeading = <h1 id="header">Hello world from JSX</h1>;
+console.log(jsxHeading);
+
+const HeadingComponent = () => {
+  return <h3>Heading added as a component</h3>;
+};
+
+const Heading2Component = () => (
+  <div id="container">
+    <HeadingComponent />
+    <HeadingComponent />
+    <HeadingComponent />
+    <HeadingComponent />
+    <h3>Heading added as component without return statement</h3>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //root.render(element);
-root.render(nested);
+//root.render(nested);
+
+root.render(<Heading2Component />);

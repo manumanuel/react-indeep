71d1e2a38d8124c0a1a00499904c199905dca98a
Manu Manuel <manuelmanu008@gmail.com>
2024-07-13 19:41:58 +0530

    useEffect for Api call, state mangement variable handling

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,19 +1,69 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { resData } from "../utils/sampleData";
 import RestaurantCard from "./RestaurantCard";
-9;
-const BodyComponent = () => {
-  let [filteredRestaurants, setFilteredRestaurants] = useState(resData);
+import ShimmerComponent from "./ShimmerComponent";
 
-  return (
+const BodyComponent = () => {
+  let [listedRestaurants, setListedRestaurants] = useState([]);
+  let [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchRestaurant, setSearchRestaurant] = useState("");
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+  const fetchRestaurants = async () => {
+    const loadRestaurants = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/search/v3?lat=9.9312328&lng=76.26730409999999&str=Chicken%20Biryani&trackingId=undefined&submitAction=ENTER&queryUniqueId=74ac7a6e-0bee-06d7-b24a-adac26b3222f&selectedPLTab=RESTAURANT"
+    );
+    const resJson = await loadRestaurants.json();
+    // console.log(
+    //   resJson.data.cards[0].groupedCard.cardGroupMap.RESTAURANT.cards
+    // );
+    setListedRestaurants(
+      resJson?.data?.cards[0]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards
+    );
+    setFilteredRestaurants(
+      resJson?.data?.cards[0]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards
+    );
+  };
+
+  //conditional rendering
+  // if (filteredRestaurants.length === 0) {
+  //   return <ShimmerComponent />;
+  // }
+
+  return filteredRestaurants.length === 0 ? (
+    <ShimmerComponent />
+  ) : (
     <div className="body">
       <div className="search">
+        <input
+          type="text"
+          value={searchRestaurant}
+          onChange={(e) => {
+            setSearchRestaurant(e.target.value);
+          }}
+        />
         <button
           className="filter-btn"
           onClick={() => {
-            const result = filteredRestaurants.filter(
-              (x) => x.card.card.info.avgRating > 4
+            filteredRestaurants = listedRestaurants.filter((res) =>
+              res.card.card.info.name
+                .toLowerCase()
+                .includes(searchRestaurant.toLowerCase())
+            );
+            setFilteredRestaurants(filteredRestaurants);
+          }}
+        >
+          Search
+        </button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const result = listedRestaurants.filter(
+              (x) => x.card.card.info.avgRating > 4.5
             );
             setFilteredRestaurants(result);
             //console.log(result);
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,19 +1,35 @@
+import { useState } from "react";
+
 import { LOGO_URL } from "../utils/constants";
 
-const HeaderComponent = () => (
-  <div className="header">
-    <div className="logo-container">
-      <img className="logo" src={LOGO_URL} />
+const HeaderComponent = () => {
+  const [btnLogInOut, setBtnLogInOut] = useState("Login");
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="btnLogBtn"
+            onClick={() => {
+              btnLogInOut === "Login"
+                ? setBtnLogInOut("Logout")
+                : setBtnLogInOut("Login");
+            }}
+          >
+            {btnLogInOut}
+          </button>
+        </ul>
+      </div>
     </div>
-    <div className="nav-items">
-      <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 export default HeaderComponent;
